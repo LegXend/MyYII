7fb51462ce6260ba9f2aa87ce7f16eae46c47519
liuxu <liuxu@ktjr.com>
2017-08-17 15:10:25 +0800

    增加status

--- basic/models/Status.php
+++ basic/models/Status.php
@@ -2,31 +2,69 @@
 
 namespace app\models;
 
+use Yii;
 use yii\base\Model;
 
-class Status extends Model
+/**
+ * This is the model class for table "status".
+ *
+ * @property integer $id
+ * @property string $message
+ * @property integer $permissions
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Status extends \yii\db\ActiveRecord
 {
-  const PERMISSIONS_PRIVATE = 10;
-  const PERMISSIONS_PUBLIC = 20;
+    const PERMISSIONS_PRIVATE = 10;
+    const PERMISSIONS_PUBLIC = 20; 
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'status';
+    }
 
-  public $text;
-  public $permissions;
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['message'], 'string'],
+            [['permissions', 'created_at', 'updated_at'], 'integer'],
+            [['created_at', 'updated_at'], 'required'],
+        ];
+    }
 
-  public function rules()
-  {
-    return[
-        [['text', 'permissions'], 'required']
-    ];
-  }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'message' => 'Message',
+            'permissions' => 'Permissions',
+            // 'created_at' => 'Created At',
+            // 'updated_at' => 'Updated At',
+        ];
+    }
 
-  public function getPermissions() {
-    return array (self::PERMISSIONS_PRIVATE=>'Private',self::PERMISSIONS_PUBLIC=>'Public');
-  }
 
-  public function getPermissionsLabel($permissions) {
-    if ($permissions==self::PERMISSIONS_PUBLIC) {
-      return 'Public';
-    } else {
-      return 'Private';        
-  }
+    public function getPermissions() 
+    {
+      return array (self::PERMISSIONS_PRIVATE=>'Private', self::PERMISSIONS_PUBLIC=>'Public');
+    }
+     
+    public function getPermissionsLabel($permissions) 
+    {
+      if ($permissions == self::PERMISSIONS_PUBLIC) 
+      {
+        return 'Public';
+      } else {
+        return 'Private';        
+      }
+    }
 }
